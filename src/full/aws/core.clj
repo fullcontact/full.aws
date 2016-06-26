060b192d8cb29791c89eae3ea67042218d18a4bc
Kaspars Dancis <kaspars@fullcontact.com>
2016-06-26 17:52:20 -0600

    S3 - support custom endpoint (for example for local dev mode using S3 emulator)

--- src/full/aws/core.clj
+++ src/full/aws/core.clj
@@ -2,7 +2,7 @@
   (:require [full.core.config :refer [opt]])
   (:import (com.amazonaws ClientConfiguration Protocol)
            (com.amazonaws.regions Region Regions)
-           (com.amazonaws.services.s3 AmazonS3Client)
+           (com.amazonaws.services.s3 AmazonS3Client S3ClientOptions)
            (com.amazonaws.auth AWSCredentialsProvider
                                BasicAWSCredentials
                                DefaultAWSCredentialsProviderChain)))
@@ -10,6 +10,7 @@
 (def region-name   (opt [:aws :region]))
 (def client-id     (opt [:aws :client-id] :default nil))
 (def client-secret (opt [:aws :client-secret] :default nil))
+(def s3-endpoint   (opt [:aws :s3-endpoint] :default nil))
 
 (def region (delay (Region/getRegion (Regions/fromName @region-name))))
 
@@ -26,7 +27,17 @@
   (delay (-> (ClientConfiguration.) (.withProtocol (Protocol/HTTP)))))
 
 (def default-https-client
-  (delay (doto (AmazonS3Client. @credentials-provider) (.setRegion @region))))
+  (delay (let [c (AmazonS3Client. @credentials-provider)]
+           (.setRegion c @region)
+           (when @s3-endpoint
+             (.setEndpoint c @s3-endpoint)
+             (.setS3ClientOptions c (-> (S3ClientOptions.) (.withPathStyleAccess true))))
+           c)))
 
 (def default-http-client
-  (delay (doto (AmazonS3Client. @credentials-provider @http-protocol-client-config) (.setRegion @region))))
+  (delay (let [c (AmazonS3Client. @credentials-provider @http-protocol-client-config)]
+           (.setRegion c @region)
+           (when @s3-endpoint
+             (.setEndpoint c @s3-endpoint)
+             (.setS3ClientOptions c (-> (S3ClientOptions.) (.withPathStyleAccess true))))
+           c)))
