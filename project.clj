d9176d7aa98988f7c2f2320cd9b048175b2f6466
Kaspars Dancis <kaspars@fullcontact.com>
2016-06-26 18:00:48 -0600

    0.8.22-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.aws "0.8.21"
+(defproject fullcontact/full.aws "0.8.22-SNAPSHOT"
   :description "Async Amazon Webservices client."
 
   :url "https://github.com/fullcontact/full.monty"
