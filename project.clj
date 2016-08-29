5fa3499adaf7c687b6d714cb7823a169efae20dc
gazz <janis@fullcontact.com>
2016-08-29 14:48:41 -0600

    Version 0.10.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.aws "0.10.4"
+(defproject fullcontact/full.aws "0.10.5-SNAPSHOT"
   :description "Async Amazon Webservices client."
   :url "https://github.com/fullcontact/full.aws"
   :license {:name "Eclipse Public License - v 1.0"
